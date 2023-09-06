25df47565554f85562dca3ca0f8ac2746776d301
Adam Hjelm <adam.hjelm.special@gmail.com>
2023-09-06 10:37:37 +0200

    Updated & Added XR Plugins & Device Manager

---
+++ Assets/Scripts/ConfigureBoundary.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Management;
+
+public class ConfigureBoundary : MonoBehaviour
+{
+    //    public GameObject wallMarker;
+    //    //Check if the boundary is configured
+    //    bool configured = OVRManager.boundary.GetConfigured();
+    //        if (configured)
+    //        {
+    //            //Grab all the boundary points. Setting BoundaryType to OuterBoundary is necessary
+    //            Vector3[] boundaryPoints = OVRManager.boundary.GetGeometry(OVRBoundary.BoundaryType.OuterBoundary);
+
+    //             //Generate a bunch of tall thin cubes to mark the outline
+    //            foreach (Vector3 pos in boundaryPoints)
+    //            {      
+    //                Instantiate(wallMarker, pos, Quaternion.identity);
+    //}
+
+    //private void Start()
+    //{
+    //    XRInputSubsystem.
+    //}
+
+}
